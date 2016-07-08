7087a1e021e18075eaaeca1ca02ce108e08a4c3e
flashpratt <flashpratt@gmail.com>
2016-07-07 20:17:45 -0500

    role.storekeeper: cleaned up some code; added TODO

--- ares/role.storekeeper.js
+++ ares/role.storekeeper.js
@@ -1,30 +1,30 @@
 //a creep who harvests energy and stores it in thean adjacent container
 var roleStorekeeper = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        var arrived = false;
-        var sources = creep.room.find(FIND_SOURCES);
-        var target = sources[0];
-        var storageTarget = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-            filter: function(object){
-                return object.structureType == STRUCTURE_CONTAINER;
-            }
-        });
-        
-        if(creep.pos.isNearTo(target)) {
-            var arrived = true;
-        }
-        if(arrived == false){
-            creep.moveTo(target);
-        }
-        if(creep.energy > creep.energyCapacity/2){
-            creep.transferEnergy(storageTarget)
-        }
-        if(arrived == true){
-            creep.harvest(target)
-        }
+  /** @param {Creep} creep **/
+  run: function(creep) {
+
+    //if we have no energy, set task to recharge
+    if(creep.carry.energy === 0) {
+          creep.memory.task = 'recharge';
     }
+    //if we have full energy, set task to store energy
+    if(creep.carry.energy >= creep.carryCapacity) {
+          creep.memory.task = 'chargeStorage';
+    }
+    //if we are recharging, find sources and harvest or move if out of range
+    if(creep.memory.task == 'recharge') {
+          var sources = creep.room.find(FIND_SOURCES);
+          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources[0]);
+          }
+      }
+    //if we are chargingStorage, find nearest uncharged storage and charge it
+    else if (creep.memory.task == 'chargeStorage') {
+      //TO DO: add code here
+    }
+}
 };
+
 
 module.exports = roleStorekeeper;
