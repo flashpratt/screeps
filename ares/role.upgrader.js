dda69f88b9caa4b29add83605803fa33105e05c6
flashpratt <flashpratt@gmail.com>
2016-07-07 20:02:09 -0500

    Fixed upgraders; now charge fully before upgrading

--- ares/role.upgrader.js
+++ ares/role.upgrader.js
@@ -3,17 +3,28 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
+
+      //if we have no energy, set task to recharge
+      if(creep.carry.energy === 0) {
+            creep.memory.task = 'recharge';
+      }
+      //if we have full energy, set task to upgrade
+      if(creep.carry.energy >= creep.carryCapacity) {
+            creep.memory.task = 'upgrade';
+      }
+      //if we are recharging, find sources and harvest or move if out of range
+	    if(creep.memory.task == 'recharge') {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
-        else {
+      //if we are upgrading, find the room controller and upgrade or move if out of range
+      else if (creep.memory.task == 'upgrade') {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
-        }
+      }
 	}
 };
 
